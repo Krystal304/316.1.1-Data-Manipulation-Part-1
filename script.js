8aef8c6abcd81f3be31f716e8e0e4ec8d0cf2d5c
Krystal304 <krystalwilliams864@gmail.com>
2024-09-21 19:57:36 -0400

    part

--- script.js
+++ script.js
@@ -19,12 +19,10 @@ var menuLinks = [
     ]},
   ];
 
-
-
-
-
+// Part 1
 // Select and cache the <main> element in a variable named mainEl.
-let mainEl = document.querySelector('main');
+let mainEl = document.querySelector("main");
+
 
 // Set the background color of mainEl to the value stored in the --main-bg CSS custom property.
 // Hint: Assign a string that uses the CSS var() function like this: 'var(--main-bg)'.
@@ -47,66 +45,94 @@ let topMenuEl = document.getElementById(`top-menu`)
 topMenuEl.style.height = '100%';
 
 // Set the background color of topMenuEl to the value stored in the --top-menu-bg CSS custom property.
-topMenuEl.style.backgroundColor = `var(--top-menu-bg)`
+topMenuEl.style.backgroundColor = `var(--top-menu-bg)`;
 
 // Add a class of flex-around to topMenuEl.
-topMenuEl.classList.add('flex-around')
+topMenuEl.classList.add('flex-around');
 
 // Iterate over the entire menuLinks array and for each "link" object:
-menuLinks.forEach((link) => { 
+menuLinks.forEach((link) =>{ 
 
 // Create an <a> element.     - .create
-let newLink = document.createElement(`a`)
+let newLink = document.createElement(`a`);
 
 
 // On the new element, add an href attribute with its value set to the href property of the "link" object.
-newLink.setAttribute(`href`, link.href)
-
-
+newLink.setAttribute(`href`, link.href);
 
 
 // Set the new element's content to the value of the text property of the "link" object.
-newLink.textContent = link.text
-
-
+newLink.textContent = link.text;
 
 // Append the new element to the topMenuEl element.
 topMenuEl.appendChild(newLink);
 });
 
+// Part 3
 // Select and cache the <nav id="sub-menu"> element in a variable named subMenuEl.
-let subMenuEl = document.getElementById('sub-menu');
+// let subMenuEl = document.getElementById('sub-menu');
 
 // Set the height subMenuEl element to be "100%".
-subMenuEl.style.height = '100%';
+// subMenuEl.style.height = '100%';
 
 // Set the background color of subMenuEl to the value stored in the --sub-menu-bg CSS custom property.
-subMenuEl.style.backgroundColor = (`var(--sub-menu-bg)`);
+// subMenuEl.style.backgroundColor = (`var(--sub-menu-bg)`);
 
 
 // Add the class of flex-around to the subMenuEl element
-subMenuEl.classList.add(`flex-around`)
+// subMenuEl.classList.add(`flex-around`);
 
 // Set the CSS position property of subMenuEl to the value of absolute.
-subMenuEl.getElementById(`sub-menu`).style.position = `absolute`;
+// subMenuEl.style.position = `absolute`;
 
 // Set the CSS top property of subMenuEl to the value of 0.
-subMenuEl.getElementById(`sub-menu`).style.top = 0;
+// subMenuEl.style.top = 0;
 
+
+// Part 4
 // Select and cache the all of the <a> elements inside of topMenuEl in a variable named topMenuLinks.
-
-let topMenuLinks = document.querySelectorAll(`a`)
+let topMenuLinks = topMenuEl.querySelectorAll(`a`);
 
 
 // Attach a delegated 'click' event listener to topMenuEl.
-function handleClick(event) {
+const cBtn = document.getElementById(`myBtn`);
+
+topMenuEl.addEventListener("click", function(event) {
+ 
+
+
 // The first line of code of the event listener function should call the event object's preventDefault() method.
-    event.preventDefault();
+
+event.preventDefault();
+
 
 // The second line of code of the function should immediately return if the element clicked was not an <a> element.
-    
-
-// Log the content of the <a> to verify the handler is working.cBtn.addEventListener(`click`, callback);
-
-    consol
+if (event.target.tagName !== `A`){
+    return;
 }
+// Log the content of the <a> to verify the handler is working.cBtn.addEventListener(`click`, callback);
+console.log(`You clicked the right link`);
+});
+
+// The event listener should add the active class to the <a> element that was clicked, 
+// unless it was already active, in which case it should remove it.
+
+let clickedLink = (e).target;
+topMenuLinks.forEach((link) => {
+    link.classList.remove(`active`);
+
+});
+
+// The event listener should remove the active class from each other <a> element in topMenuLinks 
+// - whether the active class exists or not.
+
+// Hint: Removing a non-existent class from an element does not cause an error!
+
+// Within the event listener, if the clicked <a> element does not yet have a class of "active" 
+// (it was inactive when clicked):
+// If the clicked <a> element's "link" object within menuLinks 
+// has a subLinks property (all do, except for the "link" object for ABOUT), 
+// set the CSS top property of subMenuEl to 100%.
+// Otherwise, set the CSS top property of subMenuEl to 0.
+// Hint: Caching the "link" object will come in handy for passing its 
+// subLinks array later.
